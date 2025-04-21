606f5dda7de5274456458993ea6d7fef8310519c
igor_mendonca@outlook.com.br <igor_mendonca@outook.com.br>
2025-04-21 20:14:16 -0300

    update doc

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,28 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strings"
 )
 
+type RouteConfig struct {
+	Path   string `json:"path"`
+	Target string `json:"target"`
+}
+
+type GatewayConfig struct {
+	Routes []RouteConfig `json:"routes"`
+}
+
 func main() {
-	http.Handle("/users/", reverseProxy("http://localhost:5500"))
-	http.Handle("/products/", reverseProxy("http://localhost:3002"))
-	http.HandleFunc("/get", getHandler) // Adicionando o endpoint GET
+	// Carrega a configuração do arquivo JSON
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Erro ao carregar configuração: %v", err)
+	}
+
+	// Configura as rotas dinamicamente
+	for _, route := range config.Routes {
+		log.Println("Route loaded  Path: ", route.Path, " Target: ", route.Target)
+
+		if route.Target == "direct" {
+			// Rota direta (como o /get)
+			http.HandleFunc(route.Path, getHandler)
+		} else {
+			// Rota com proxy reverso
+			http.Handle(route.Path, reverseProxy(route.Target))
+		}
+	}
 
 	log.Println("API Gateway rodando na porta 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
+func loadConfig(filename string) (*GatewayConfig, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config GatewayConfig
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
 // Função para manipular as requisições GET
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	// Aqui você pode adicionar a lógica do que fazer com o GET
 	log.Println("Requisição GET recebida")
-
-	// Exemplo de resposta simples para o GET
 	w.Write([]byte("GET request was received"))
 }
 
@@ -35,7 +73,13 @@ func reverseProxy(target string) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Proxy %s", r.URL)
-		r.URL.Path = r.URL.Path[len("/"+r.URL.Path[1:strings.Index(r.URL.Path[1:], "/")+1]):] // Remove o prefixo
+		// Remove o prefixo do caminho
+		pathParts := strings.SplitN(r.URL.Path, "/", 3)
+		if len(pathParts) > 2 {
+			r.URL.Path = "/" + pathParts[2]
+		} else {
+			r.URL.Path = "/"
+		}
 		proxy.ServeHTTP(w, r)
 	})
 }
